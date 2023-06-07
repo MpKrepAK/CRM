e1b60dae8ec35a01e82297862de8f4bdea6300ee
111vv <111vvv03@mail.ru>
2023-06-07 20:17:27 +0300

    qweqwewq

---
+++ CRM/Views/Windows/CrmMainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace CRM.Views.Windows;
+
+public partial class CrmMainWindow : Window
+{
+    public CrmMainWindow()
+    {
+        InitializeComponent();
+    }
+}
